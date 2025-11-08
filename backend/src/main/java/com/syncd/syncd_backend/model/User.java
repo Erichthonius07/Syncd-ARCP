3f49ef28f398ad6402e8dcdfd7bd0126a66668c2
NehalAjmal <2k24.cs1k.2410488@gmail.com>
2025-11-08 16:41:41 +0530

    a fully functional User model, register/login system, and connected PostgreSQL database.

--- backend/src/main/java/com/syncd/syncd_backend/model/User.java
+++ backend/src/main/java/com/syncd/syncd_backend/model/User.java
@@ -1,15 +1,9 @@
 package com.syncd.syncd_backend.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
 
     @Id
@@ -24,4 +18,32 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    // --- Constructors ---
+    public User() {}
+
+    public User(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    // --- Getters ---
+    public Long getId() { return id; }
+
+    public String getUsername() { return username; }
+
+    public String getEmail() { return email; }
+
+    public String getPassword() { return password; }
+
+    // --- Setters ---
+    public void setId(Long id) { this.id = id; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public void setPassword(String password) { this.password = password; }
 }

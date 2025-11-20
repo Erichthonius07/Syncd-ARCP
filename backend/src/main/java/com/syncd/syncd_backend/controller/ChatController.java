b62d08c57d0d5e448b5024d74f2e07ca107cb11a
NehalAjmal <2k24.cs1k.2410488@gmail.com>
2025-11-20 14:37:51 +0530

    Update ChatController: correct method calls

--- backend/src/main/java/com/syncd/syncd_backend/controller/ChatController.java
+++ backend/src/main/java/com/syncd/syncd_backend/controller/ChatController.java
@@ -3,10 +3,9 @@ package com.syncd.syncd_backend.controller;
 import com.syncd.syncd_backend.dto.ChatMessage;
 import com.syncd.syncd_backend.model.Message;
 import com.syncd.syncd_backend.service.ChatService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +18,7 @@ public class ChatController {
     private final ChatService chatService;
     private final SimpMessagingTemplate messagingTemplate;
 
+    @Autowired
     public ChatController(ChatService chatService, SimpMessagingTemplate messagingTemplate) {
         this.chatService = chatService;
         this.messagingTemplate = messagingTemplate;
@@ -26,13 +26,9 @@ public class ChatController {
 
     @MessageMapping("/chat")
     public void receiveMessage(ChatMessage chatMessage) {
-        // Get sender from authenticated session
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String sender = auth != null ? auth.getName() : "anonymous";
-        
-        chatService.saveMessage(sender, chatMessage.getTo(), chatMessage.getText());
+        chatService.saveMessage(chatMessage.getSender(), chatMessage.getRecipient(), chatMessage.getContent());
         messagingTemplate.convertAndSendToUser(
-                chatMessage.getTo(),
+                chatMessage.getRecipient(),
                 "/queue/messages",
                 chatMessage
         );
